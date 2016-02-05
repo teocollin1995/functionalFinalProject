bab7094b688b30daa29b5d985e9fbbf774fa80b4
Teodoro Fields Collin <teocollin1995@gmail.com>
2016-02-05 17:55:48 -0600

    it compiles, but I've not tested simpleDet

--- Linear.elm
+++ Linear.elm
@@ -4,6 +4,7 @@ import Array exposing (Array)
 import Complex
 import Debug
 
+
 dimV : Vector a -> Int
 dimV m = Array.length m
 
@@ -42,34 +43,38 @@ unsafeGetM row col m = (unsafeGet col (unsafeGet row m))
 
 
 --returns the minor and the member of the top row at x
-minor : Int  -> Matrix a  -> (a, Matrix a)
+minor : Int  -> Matrix a  -> (a, Float, Matrix a)
 minor x m  = 
   let
     (rows, cols) = dimM m
     top = unsafeGetM 0 x m
     minusTop = Array.slice 1 (rows) m --I'm not sure how efficient slice is or really anything about this library
     minusx = Array.map (\m -> Array.append (Array.slice 0 x m) (Array.slice (x+1) cols m)) minusTop
+    sign = if (x % 2 == 0) then 1.0 else (-1.0)
   in
-    (top, minusx)
+    (top, sign, minusx)
     
-minors : Matrix a -> Array (a, Matrix a)
+minors : Matrix a -> Array (a, Float, Matrix a) 
 minors m =
   Array.indexedMap (\a throw -> minor a m) m
 
+
 -- in progress, but I thought I'd commit..
--- simpleDet : Matrix (Complex.Complex) -> Maybe Float
--- simpleDet m = 
---   let 
---     (rows, cols) = (dimM m)
---   in
---     if rows == 0 || (rows \= cols) then Nothing
---     else if (rows == 2) then 
---            let
---              a = unsafeGetM 0 0 m
---              b = unsafeGetM 0 1 m 
---              c = unsafeGetM 1 0 m
---              d = unsafeGetM 1 1 m 
---            in
---              Just (a*d - b*c)
---          else
---            --Array.foldr ( \(x,m) b -> b + x * simpleDet m ) 0
+simpleDet : Matrix (Expression.Complex) -> Expression.Complex
+simpleDet m = 
+  let 
+    (rows, cols) = (dimM m)
+  in
+    if rows == 0 || (rows /= cols) then Debug.crash "We are going to wrap this to make this test...."
+    else if (rows == 2) then 
+           let
+             a = unsafeGetM 0 0 m
+             b = unsafeGetM 0 1 m 
+             c = unsafeGetM 1 0 m
+             d = unsafeGetM 1 1 m 
+           in
+             (a `Complex.mult` d `Complex.sub` b `Complex.mult` c)
+         else
+           Array.foldr (\(p, sgn, sub) start -> start `Complex.add` (Complex.fromReal sgn) `Complex.mult` p `Complex.mult` (simpleDet sub)) (Complex.fromReal 0) (minors m)
+           
+
