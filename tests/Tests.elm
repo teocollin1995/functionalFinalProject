f0fb4e7fd925d2b687f4f1aac2e130a5eaa6e204
Teodoro Fields Collin <teocollin1995@gmail.com>
2016-02-23 21:48:30 -0600

    another minor fix

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,7 +4,7 @@ import Linear as L
 --https://github.com/Bogdanp/elm-combine/blob/2.0.1/tests/TestRunner.elm testing structure
 
 linearSuite : Test
-linearSuite = suite "" [test "Try" (equiv 1 1)]
+linearSuite = suite "" [test "Try" (assert True)]
 
 
 allTests : Test
